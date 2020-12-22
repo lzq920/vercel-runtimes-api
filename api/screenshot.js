33b2dc2ed429c9c7b192ea715a903a32359da6c8
eleven <lzq920@vip.qq.com>
2020-12-22 21:16:41 +0800

    fix

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,14 +1,23 @@
-const puppeteer = require('puppeteer');
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 module.exports = async (req, res) => {
-    const { url = "https://vercel-runtimes-api.vercel.app", width = "750px", height = "1334px" } = req.query;
-    const browser = await puppeteer.launch();
+    const { url = "https://vercel-runtimes-api.vercel.app", viewportWidth = "750px", viewportHeight = "1334px", type, quality, fullPage } = req.query;
+    const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+        defaultViewport: {
+            width: viewportWidth,
+            height: viewportHeight
+        }
+    });
     const page = await browser.newPage();
     await page.setViewport({
         width,
         height
     });
     await page.goto(url, { waitUntil: 'networkidle0' });
-    const screenshot = await page.screenshot();
+    const screenshot = await page.screenshot({ type, quality, fullPage });
     await browser.close();
     res.setHeader('content-type', 'application/octet-stream');
     res.send(Buffer.from(screenshot));
