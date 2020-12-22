a3cfb4e2e87077e30da5fa851fb7780240e4f7ec
eleven <lzq920@vip.qq.com>
2020-12-22 21:02:01 +0800

    fix bug

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,18 +1,7 @@
 const puppeteer = require('puppeteer');
-const { join } = require('path');
-const { tmpdir } = require('os');
-const tmpPath = tmpdir();
-const chromePath = join(tmpPath, '.local-chromium');
 module.exports = async (req, res) => {
-    const browserFetcher = puppeteer.createBrowserFetcher({
-        path: chromePath,
-    })
-    const revisionInfo = await browserFetcher.download('818858')
     const { url = "https://vercel-runtimes-api.vercel.app", width = "750px", height = "1334px" } = req.query;
-    const browser = await puppeteer.launch({
-        headless: false,
-        executablePath: revisionInfo.executablePath,
-    })
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({
         width,
