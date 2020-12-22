5fbe416ec8af5247720e094aba184b8f51cf02a5
eleven <lzq920@vip.qq.com>
2020-12-22 22:36:35 +0800

    fix

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,24 +1,25 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+// const chrome = require('chrome-aws-lambda');
+// const puppeteer = require('puppeteer-core');
 module.exports = async (req, res) => {
-    const { url = "https://vercel-runtimes-api.vercel.app", width = 750, height = 1334 } = req.query;
-    const browser = await puppeteer.launch({
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-        defaultViewport: {
-            width: width,
-            height: height
-        }
-    });
-    const page = await browser.newPage();
-    await page.setViewport({
-        width,
-        height
-    });
-    await page.goto(url);
-    const screenshot = await page.screenshot();
-    await browser.close();
-    res.setHeader('content-type', 'application/octet-stream');
-    res.send(Buffer.from(screenshot));
+    // const { url = "https://vercel-runtimes-api.vercel.app", width = 750, height = 1334 } = req.query;
+    // const browser = await puppeteer.launch({
+    //     args: chrome.args,
+    //     executablePath: await chrome.executablePath,
+    //     headless: chrome.headless,
+    //     defaultViewport: {
+    //         width: width,
+    //         height: height
+    //     }
+    // });
+    // const page = await browser.newPage();
+    // await page.setViewport({
+    //     width,
+    //     height
+    // });
+    // await page.goto(url);
+    // const screenshot = await page.screenshot();
+    // await browser.close();
+    // res.setHeader('content-type', 'application/octet-stream');
+    // res.send(Buffer.from(screenshot));
+    res.send("Hello World")
 }
