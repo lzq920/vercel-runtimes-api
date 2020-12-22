c3d339aa8a8e1e7971855790058e8dd631356b9f
Eleven <lzq920@vip.qq.com>
2020-12-22 17:18:04 +0800

    update

--- api/qrcode.js
+++ api/qrcode.js
@@ -1,7 +1,20 @@
 const QRCode = require('qrcode')
 module.exports = (req, res) => {
-    const { text } = req.query;
-    QRCode.toDataURL(text, function (err, url) {
-        res.send(url);
-    });
+    const generateQR = async (text, options) => {
+        return await QRCode.toDataURL(text, options)
+    }
+    const qrcode = async () => {
+        let {text, size, margin, dark, light } = req.query;
+        return await generateQR(text, {
+            width: size,
+            margin,
+            color: {
+                dark,
+                light
+            }
+        })
+    }
+    let url = await qrcode();
+    res.type = 'png'
+    res.body = Buffer.from(url.split(',')[1], 'base64')
 }
