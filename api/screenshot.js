8692fecf6da9f8473233c2b44da9620653ce07c5
Eleven <lzq920@vip.qq.com>
2021-01-04 12:30:04 +0800

    fix

--- api/screenshot.js
+++ api/screenshot.js
@@ -1,25 +1,12 @@
-const {
-    webkit,
-    devices
-} = require('playwright-webkit');
-const iPhone = devices['iPhone 6'];
+const { webkit } = require('playwright-webkit');
 module.exports = async (req, res) => {
-    const { url = "https://baidu.com"} = req.query;
+    const { url = "https://baidu.com" } = req.query;
     const browser = await webkit.launch();
-    const context = await browser.newContext({
-        ...iPhone,
-        permissions: ['geolocation'],
-        geolocation: {
-            latitude: 52.52,
-            longitude: 13.39
-        },
-        colorScheme: 'dark',
-        locale: 'de-DE'
-    });
+    const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto(url);
     const screenshot = await page.screenshot();
     await browser.close();
-    res.setHeader('content-type', 'application/octet-stream');
-    res.send(Buffer.from(screenshot));
+    await res.setHeader('content-type', 'application/octet-stream');
+    await res.send(Buffer.from(screenshot));
 }
