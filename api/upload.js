8cad0852f250fd178a6a079de533b64eddc842b0
Eleven <lzq920@vip.qq.com>
2021-01-05 11:07:06 +0800

    fix key

--- api/upload.js
+++ api/upload.js
@@ -1,12 +1,20 @@
 const qiniu = require("qiniu");
 module.exports = (req, res) => {
-    const accessKey = process.env.accessKey;
-    const secretKey = process.env.secretKey;
-    const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-    const options = {
-        scope: process.env.bucket,
-    };
-    const putPolicy = new qiniu.rs.PutPolicy(options);
-    const uploadToken = putPolicy.uploadToken(mac);
-    res.send({token:uploadToken});
+    const { key } = req.query;
+    if (key && key === process.env.key) {
+        const accessKey = process.env.accessKey;
+        const secretKey = process.env.secretKey;
+        const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+        const options = {
+            scope: process.env.bucket,
+        };
+        const putPolicy = new qiniu.rs.PutPolicy(options);
+        const uploadToken = putPolicy.uploadToken(mac);
+        res.send({ token: uploadToken });
+    }else{
+        res.status = 400;
+        res.send({
+            message:"缺少key或者key错误"
+        });
+    }
 }
